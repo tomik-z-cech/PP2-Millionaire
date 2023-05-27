54e293880e247093725882a0ca9bab926c173be4
tomik-z-cech <>
2023-05-27 00:30:28 +0000

    Add structure to basic JS functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,44 @@
+let playerName = '';
 console.log('Script loaded ...');
+document.getElementById('play-button').addEventListener('click', checkName);
 
-document.getElementById('play-button').addEventListener('click', myFunction);
+// Function validates users input of name
+function checkName(){
+    if (document.getElementById('player-name-input').value === '') {
+        console.log('Name empty !')
+        alert('Please enter your name !')
+    }else{
+        playerName = document.getElementById('player-name-input').value;
+        console.log('Name OK ...')
+        console.log('Starting game ...')
+        prepareUserView();
+    }
+}
 
-function myFunction(){
-    alert('jaj ty kokot');
-};
+// Function clears display area
+function clearDisplayArea(){
+    console.log('Clearing display area ...');
+    document.getElementById('display-container').innerHTML = '';
+}
+
+function createLifelines(){
+    console.log('Creating lifelines ...');
+}
+
+function createMoneyBar(){
+    console.log('Creating Money Bar ...');
+}
+
+function welcomePlayer(){
+    console.log('Displaying welcome message ...');
+    document.getElementById('display-container').innerHTML = `Hello ${playerName} !`;
+}
+
+function prepareUserView(){
+    clearDisplayArea();
+    document.getElementById('display-container').style.backgroundImage = "url('assets/images/game-background.jpg')";
+    welcomePlayer();
+    createLifelines();
+    createMoneyBar();
+}
+
